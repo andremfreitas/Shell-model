9223c9b82b0d88735731bc4ac32b96907087ea8e
andremfreitas <andre.marreiros.freitas@gmail.com>
2024-01-05 16:02:59 +0000

    Added kolmogorov time scale

--- fortran/params.py
+++ fortran/params.py
@@ -6,8 +6,12 @@ nu = 1e-6
 eta = (nu**3 / dn)**0.25     # lenght scale
 k_eta = 1 / eta
 
-print('Kolmogorov wavenumber:', k_eta)
+print('Kolmogorov wavenumber:', "{:e}".format(k_eta))
 
 n = math.log2(k_eta)
 
-print(f"The power of 2 closest to {k_eta} is 2^{round(n)}")
+print(f"The power of 2 closest to {k_eta:e} is 2^{round(n)}")
+
+tau_eta = (nu / dn) ** 0.5 
+
+print('Kolmogorov time scale:', "{:e}".format(tau_eta))
