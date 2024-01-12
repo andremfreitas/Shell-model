bfd8db0f90f99705b95829a24d92d31ccbebd4a9
andremfreitas <andre.marreiros.freitas@gmail.com>
2024-01-12 12:08:53 +0100

    update

--- python/code.py
+++ python/code.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, MaxNLocator
 
 ####################################################################
 ######## Fixed Parameters & Useful vectors #########################
@@ -13,7 +14,7 @@ for n in range(N):
     ek.append(np.exp(-nu*dt*k[n]*k[n]/2.0))
     if n==0 :
         forcing.append(1+1j)
-Tmax=10.0
+Tmax=100.0
 time=np.arange(0,int(Tmax/dt))
 ####################################################################
 ####################################################################
@@ -72,12 +73,62 @@ for n in range(6):  # Only the first 6 shells are not zero
 for t in time-1: 
     u[:,t+1]=RK4(u[:,t])
 
+    
+for t in time-1:
+    u[15,t+1] = u[14,t+1] / 3
+    u[16,t+1] = u[15,t+1] / 3
+    u[17,t+1] = u[16,t+1] / 3
+    u[18,t+1] = u[17,t+1] / 3
+    u[19,t+1] = u[18,t+1] / 3
 
-plt.plot(2*time*dt,np.real(u[4,:]),label=r'$u_5(t)$')
-plt.plot(2*time*dt,np.real(u[10,:]),label=r'$u_11(t)$')
-plt.xlabel(r'$t/\tau_0$')
-plt.ylabel(r'$u_0(t)$')
-plt.savefig("time_VS_u")
-plt.legend(loc="best")
-plt.show()
+
+print(u.shape) # -- n * time-1
+
+
+
+# plt.plot(2*time*dt,np.real(u[4,:]),label=r'$u_5(t)$')
+# plt.plot(2*time*dt,np.real(u[10,:]),label=r'$u_11(t)$')
+# plt.xlabel(r'$t/\tau_0$')
+# plt.ylabel(r'$u_0(t)$')
+# plt.savefig("time_VS_u")
+# plt.legend(loc="best")
+# plt.show()
+
+# u[15,t+1] = u[14,t+1] / 3
+# u[16,t+1] = u[15,t+1] / 3
+# u[17,t+1] = u[16,t+1] / 3
+# u[18,t+1] = u[17,t+1] / 3
+# u[19,t+1] = u[18,t+1] / 3
+
+u_avg = np.mean(u[:, int((len(time)+1)/2):], axis=1)
+print(u_avg.shape)
+
+s1 = np.sqrt(np.real(u_avg*np.conj(u_avg)))
+s2 = np.sqrt(np.real(u_avg*np.conj(u_avg)))**2
+s3 = np.sqrt(np.real(u_avg*np.conj(u_avg)))**3
+s4 = np.sqrt(np.real(u_avg*np.conj(u_avg)))**4
+s5 = np.sqrt(np.real(u_avg*np.conj(u_avg)))**5
+s6 = np.sqrt(np.real(u_avg*np.conj(u_avg)))**6
+
+
+
+n = np.arange(1,21)
+
+plt.figure()
+plt.plot(n, s1, label = r'$S_1$', marker = 'o')
+plt.plot(n, s2, label = r'$S_2$', marker = 'o')
+plt.plot(n, s3, label = r'$S_3$', marker = 'o')
+plt.plot(n, s4, label = r'$S_4$', marker = 'o')
+plt.plot(n, s5, label = r'$S_5$', marker = 'o')
+plt.plot(n, s6, label = r'$S_6$', marker = 'o')
+# plt.plot(kn, flux, label = 'Flux    ', marker = 'o')
+plt.legend(loc='lower left')
+plt.xlabel(r'$n$', fontsize = 16)
+plt.ylabel(r'$S_n$', fontsize = 16)
+plt.gca().xaxis.set_major_locator(MultipleLocator(2))   # Set the x-axis locator to show multiples of 2
+#plt.xscale('log', base = 2)
+plt.yscale('log', base = 2)
+plt.tight_layout()
+plt.savefig('struct_functions.png')
+plt.close()
 
